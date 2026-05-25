140b92a8663097648cf15ccca0695c3e68f85f2e
Davi Abreu da Silveira <daviabreudasilveira@gmail.com>
2026-05-25 10:08:11 -0300

    initial commit

---
+++ ponderada2/ponderada2App.swift
@@ -0,0 +1,17 @@
+//
+//  ponderada2App.swift
+//  ponderada2
+//
+//  Created by Davi Abreu da Silveira on 25/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ponderada2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
